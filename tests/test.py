515d0a887a40a374cb5affca526b45a3cf7dffc2
kundzior <grzybek2413@gmail.com>
2023-07-01 22:01:07 +0200

    get productc parametrize

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,7 @@
 from app.vending_main import change_money, compare_value, get_money, get_product
-from unittest import mock 
+from unittest import mock
 import pytest
+from app.database import DB
 
 def test_get_money():
     customer_cash = 5
@@ -25,18 +26,23 @@ def test_compare_value(customer_cash, order_value, expected_result):
 @pytest.mark.parametrize(
     "customer_cash, order_value, expected_result", [(5, 5, 0), (10, 5, 5)]
 )
-def test_change_money_if_nothing_to_change(
-    customer_cash, order_value, expected_result
-):
+def test_change_money_if_nothing_to_change(customer_cash, order_value, expected_result):
     result = change_money(customer_cash, order_value)
     assert result == expected_result
 
-@mock.patch("app.database.DB")
-def test_get_product(db):
-    db.return_value = {11:1}
-    product_id = 11
-    quantity = 1
-    expected_result = "wydano produkt"
-    result = get_product(product_id, quantity)
-    assert result == expected_result
-    
+
+@pytest.mark.parametrize(
+    "product_id, quantity, expected_result",
+    [
+        (11, 1, "wydano produkt"),
+        (11, 2, "zbyt duża ilość produktu, max: 1"),
+        (12, 1, "nie znaleziono produktu"),
+    ],
+)
+def test_get_product(product_id, quantity, expected_result):
+    # product_id = 11
+    # quantity = 1
+    # expected_result = "wydano produkt"
+    with mock.patch.dict(DB, {11:1}):
+        result = get_product(product_id, quantity)
+        assert result == expected_result
