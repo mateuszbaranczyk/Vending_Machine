f842548aa6458a54bbd9e0158702f2efa1d3c170
kundzior <grzybek2413@gmail.com>
2023-07-01 21:10:01 +0200

    typo

--- tests/test.py
+++ tests/test.py
@@ -10,26 +10,23 @@ def test_get_money():
 
 
 @pytest.mark.parametrize(
-    "customer_cache, order_value, expected_result",
+    "customer_cash, order_value, expected_result",
     [
         (5, 5, "wydaje produkt"),
         (10, 5, "wydaję resztę i produkt"),
         (5, 10, "za mało hajsu"),
     ],
 )
-def test_compare_value(customer_cache, order_value, expected_result):
-    result = compare_value(customer_cache, order_value)
+def test_compare_value(customer_cash, order_value, expected_result):
+    result = compare_value(customer_cash, order_value)
     assert result == expected_result
 
 
 @pytest.mark.parametrize(
-    "customer_cache, order_value, expected_result", [(5, 5, 0), (10, 5, 5)]
+    "customer_cash, order_value, expected_result", [(5, 5, 0), (10, 5, 5)]
 )
 def test_change_money_if_nothing_to_change(
-    customer_cache, order_value, expected_result
+    customer_cash, order_value, expected_result
 ):
-    customer_cash = 5
-    order_value = 5
-    expected_result = 0
     result = change_money(customer_cash, order_value)
     assert result == expected_result
