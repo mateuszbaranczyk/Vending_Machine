55e11ace2f5d5b4d9ddca21ed9eccb31471d910f
kundzior <grzybek2413@gmail.com>
2023-07-02 19:59:56 +0200

    update quanity dataclass test

--- test/test_database.py
+++ test/test_database.py
@@ -7,7 +7,12 @@ def test_product_dataclass():
     quantity=50
 
     product= Product(product_id, unit_price, quantity)
-    
+
     assert product.product_id == product_id
     assert product.unit_price == unit_price
     assert product.quantity == quantity
+
+def test_give_quantity(product):
+    result = product.give_quantity(2)
+    expected_result = 48
+    assert result == expected_result
