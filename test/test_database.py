565a624c00ab80a17cdb7a9534b57e07cc26f20c
kundzior <grzybek2413@gmail.com>
2023-07-02 19:47:58 +0200

    product dataclass test

---
+++ test/test_database.py
@@ -0,0 +1,13 @@
+from app.database import Product
+
+
+def test_product_dataclass():
+    product_id=11
+    unit_price=10
+    quantity=50
+
+    product= Product(product_id, unit_price, quantity)
+    
+    assert product.product_id == product_id
+    assert product.unit_price == unit_price
+    assert product.quantity == quantity
