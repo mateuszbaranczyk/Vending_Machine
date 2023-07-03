1221c57ba3edd764ad8306fe290509045b3a542d
kundzior <grzybek2413@gmail.com>
2023-07-03 13:57:27 +0200

    test for main

---
+++ test/test_main.py
@@ -0,0 +1,18 @@
+from unittest.mock import patch, Mock
+from app.database import DB
+from app.vending_main import VendingMashine
+import builtins
+
+
+mocked_input = Mock()
+mocked_input.side_effect = [11, 1, 10]
+
+@patch.object(builtins, "input", mocked_input)
+def test_vending_main(product):
+    product.quantity = 1
+    with patch.dict(DB, {11: product}):
+        vending = VendingMashine()
+        vending.__call__()
+        actual_quantity = DB[11].quantity
+
+    assert actual_quantity == 0
